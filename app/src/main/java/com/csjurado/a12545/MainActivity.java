b69e16602a968a14efa324c258fc2caba3708819
csjurado <carlos.santiago.jurado.bazan@gmail.com>
2023-07-28 16:30:40 -0500

    Mensaje descriptivo

--- app/src/main/java/com/csjurado/a12545/MainActivity.java
+++ app/src/main/java/com/csjurado/a12545/MainActivity.java
@@ -35,8 +35,36 @@ public class MainActivity extends AppCompatActivity {
         String valor2=input2.getText().toString();
         int n1=Integer.parseInt(valor1);
         int n2 = Integer.parseInt(valor2);
-        int suma = n1-n2;
-        String result = String.valueOf(suma);
+        int restar = n1-n2;
+        String result = String.valueOf(restar);
+        resultadoTxt.setText(result);
+    }
+    public void mutiplicacion(View view){
+        String valor1=input1.getText().toString();
+        String valor2=input2.getText().toString();
+        int n1=Integer.parseInt(valor1);
+        int n2 = Integer.parseInt(valor2);
+        int mutiplicacion = n1 * n2;
+        String result = String.valueOf(mutiplicacion);
+        resultadoTxt.setText(result);
+    }
+    public void division(View view){
+        String valor1=input1.getText().toString();
+        String valor2=input2.getText().toString();
+        int n1=Integer.parseInt(valor1);
+        int n2 = Integer.parseInt(valor2);
+        int division = n1 / n2;
+        String result = String.valueOf(division);
+        resultadoTxt.setText(result);
+    }
+
+    public void seno(View view){
+        String valor1=input1.getText().toString();
+        String valor2=input2.getText().toString();
+        double n1=Double.parseDouble(valor1);
+        int n2 = Integer.parseInt(valor2);
+        double seno = Math.sin(n1);
+        String result = String.valueOf(seno);
         resultadoTxt.setText(result);
     }
 }
